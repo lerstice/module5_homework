57354b67440b4f7f316aa050b9392a672b8246b2
lerstice <lerstice@gmail.com>
2021-03-30 19:24:55 +0300

    Add JS-files

---
+++ task_7.js
@@ -0,0 +1,37 @@
+// Задание 7.
+// Дан массив. Нужно вывести в консоль количество чётных и нечётных элементов в массиве. Если в массиве есть нулевой элемент, то он учитывается и выводится отдельно. 
+// При выполнении задания необходимо учесть, что массив может содержать не только числа, но и, например, знаки, null и так далее.
+
+let arr = [1, 4, 5, 7, 2, 0, null];
+let sumEven = 0;
+let sumOdd = 0;
+let nullElement = 0;
+let notNumber = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] == 'number') {
+    if (arr[i] % 2 === 0 & arr[i] !== 0) {
+      sumEven += 1
+    } else {
+      if (arr[i] % 2 !== 0) {
+        sumOdd += 1
+      } else {
+        if (arr[i] === 0) {
+          nullElement = 'Массив содержит нулевой элемент'
+        }
+      }
+      }
+    } else {
+      if (typeof arr[i] !== 'number') {
+        notNumber = 'Массив содержит элементы не являющиеся числами'
+      }
+    }
+  }
+   
+console.log(`Массив содержит ${sumEven} чётных и ${sumOdd} нечётных числа`)
+if (nullElement !== 0) {
+  console.log(nullElement)
+}
+if (notNumber !== 0) {
+  console.log(notNumber)
+}
